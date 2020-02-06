b621c91f9e145d4511b7d1b649c10f3044ceb356
Robert Barrow <merefield@gmail.com>
2020-02-06 12:07:57 +0000

    FEATURE: restrict public games by Category

--- jobs/frotzbot_reply_job.rb
+++ jobs/frotzbot_reply_job.rb
@@ -11,8 +11,12 @@ class ::Jobs::FrotzBotPostReplyJob < Jobs::Base
     if bot_user && post
       message_body = nil
       is_private_msg = post.topic.private_message?
-        
-      unless (is_private_msg && !SiteSetting.frotz_permitted_in_private_messages)
+
+      permitted_categories = SiteSetting.frotz_permitted_categories.split('|')
+
+      if (is_private_msg && !SiteSetting.frotz_permitted_in_private_messages)
+        message_body = I18n.t('frotz.errors.forbiddeninprivatemessages')
+      elsif (SiteSetting.frotz_permitted_all_categories || (permitted_categories.include? post.topic.category_id.to_s))
         puts "Creating a new reply message..."
         begin
           message_body = DiscourseFrotz::FrotzBot.ask(opts)
@@ -21,7 +25,18 @@ class ::Jobs::FrotzBotPostReplyJob < Jobs::Base
           Rails.logger.error ("FroztBot: There was a problem: #{e}")
         end
       else
-        message_body = I18n.t('frotz.errors.forbiddeninprivatemessages')
+        if permitted_categories.size > 0
+          message_body = I18n.t('frotz.errors.forbiddenoutsidethesecategories')
+          permitted_categories.each_with_index do |permitted_category, index|
+            if index == permitted_categories.size - 1
+              message_body += "##{Category.find_by(id:permitted_category).slug}"
+            else
+              message_body += "##{Category.find_by(id:permitted_category).slug}, "
+            end
+          end
+        else
+          message_body = I18n.t('frotz.errors.forbiddenanycategory') 
+        end
       end
       reply_creator = DiscourseFrotz::ReplyCreator.new(user: bot_user, reply_to: post)
       reply_creator.create(message_body)
