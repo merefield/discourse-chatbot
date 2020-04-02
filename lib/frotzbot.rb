6df64af95bba6b8f66577a8aca659670474f1873
Robert Barrow <merefield@gmail.com>
2020-04-02 16:36:09 +0100

    Updates to support a frotz which provides bbcode instead of ansi codes

--- lib/frotzbot.rb
+++ lib/frotzbot.rb
@@ -4,96 +4,6 @@ module DiscourseFrotz
 
   class FrotzBot
 
-    ## ANSIname => ANSIcode LUT
-    ANSINAME2CODE= {  "reset"     => "\e[0m",    "bold"       => "\e[1m",
-      "underline" => "\e[4m",    "blink"      => "\e[5m",
-      "reverse"   => "\e[7m",    "invisible"  => "\e[8m",
-      "black"     => "\e[0;30m", "darkgrey"   => "\e[1;30m",
-      "red"       => "\e[0;31m", "lightred"   => "\e[1;31m",
-      "green"     => "\e[0;32m", "lightgreen" => "\e[1;32m",
-      "brown"     => "\e[0;33m", "yellow"     => "\e[1;33m",
-      "blue"      => "\e[0;34m", "lightblue"  => "\e[1;34m",
-      "purple"    => "\e[0;35m", "magenta"    => "\e[1;35m",
-      "cyan"      => "\e[1;36m", "lightcyan"  => "\e[1;36m",
-      "grey"      => "\e[0;37m", "white"      => "\e[1;37m",
-      "bgblack"   => "\e[40m",   "bgred"      => "\e[41m",
-      "bggreen"   => "\e[42m",   "bgyellow"   => "\e[43m",
-      "bgblue"    => "\e[44m",   "bgmagenta"  => "\e[45m",
-      "bgcyan"    => "\e[46m",   "bgwhite"    => "\e[47m"
-    }
-
-    ## BBColor => ANSIname LUT
-    BBCOLOR2ANSI  = { "skyblue"   => "blue",     "royalblue" => "blue",
-        "blue"      => "blue",     "darkblue"  => "blue",
-        "orange"    => "red",      "orangered" => "red",
-        "crimson"   => "red",      "red"       => "lightred",
-        "firebrick" => "red",      "darkred"   => "red",
-        "green"     => "green",    "limegreen" => "green",
-        "seagreen"  => "green",    "darkgreen" => "green",
-        "deeppink"  => "magenta",  "tomato"    => "red",
-        "coral"     => "cyan",     "purple"    => "purple",
-        "indigo"    => "blue",     "burlywood" => "red",
-        "sandybrown"=> "red",      "sierra"    => "sierra",
-        "chocolate" => "brown",    "teal"      => "teal",
-        "silver"    => "white",
-        "black"     => "black",    "yellow"    => "yellow",
-        "magenta"   => "magenta",  "cyan"      => "cyan",
-        "white"     => "white"
-      }
-
-    ## ANSInames => BBCode LUT
-    ANSINAME2BBCODE = { "bold" => "B", "underline" => "U", "reverse" => "I",
-
-          "red"    => "color=red",      "blue"   => "color=blue",
-          "green"  => "color=green",    "cyan"   => "color=cyan",
-          "magenta"=> "color=deeppink", "purple" => "color=purple",
-          "black"  => "color=black",    "white"  => "color=white",
-          "yellow" => "color=yellow",   "brown"  => "color=chocolate",
-          "bgblack"   => "bgcolor=black",   "bgred"      => "bgcolor=red",
-          "bggreen"   => "bgcolor=green",   "bgyellow"   => "bgcolor=yellow",
-          "bgblue"    => "bgcolor=blue",   "bgmagenta"  => "bgcolor=magenta",
-          "bgcyan"    => "bgcolor=cyan",   "bgwhite"    => "bgcolor=white"
-        }
-
-    # ---------------------------
-
-    # Returns the string with all ansi escape sequences converted to BBCodes
-    def self.ansi_to_bbcode(string)
-      return "" if string.nil? || string.to_s.strip.empty?
-      result = ""
-      tagstack = []
-
-      ## Iterate over found ansi sequences
-      string.scan(/\e\[[0-9;]+m/).each do |seq|
-        ansiname = ANSINAME2CODE.invert["#{seq}"]
-
-        ## Pop last tag and form closing tag
-        if ansiname == "reset"
-          lasttag = tagstack.pop
-          bbname = "/" + String.new( lasttag.split("=")[0] )
-
-        ## Get corresponding BBCode tag + Push to stack
-        else
-          bbname   = ANSINAME2BBCODE[ansiname]
-          tagstack.push(bbname)
-        end
-
-        ## Replace ansi sequence by BBCode tag
-        replace = sprintf("[%s]", bbname)
-        string.sub!(seq, replace)
-      end
-
-      ## Append converted line
-      result << sprintf("%s", string)
-
-      ## Some tags are unclosed
-      while !tagstack.empty?
-        result << sprintf("[/%s]", String.new(tagstack.pop.split("=")[0])  )
-      end
-
-      return result
-    end
-
     def self.strip_header_and_footer(string)
 
       lines = string.split(/\n+|\r+/)
@@ -110,18 +20,8 @@ module DiscourseFrotz
           next
         end
 
-        ## Pre-process the ASCII codes
-
-        line.gsub!("\e[0K","")  #suppress clear formats
-        line.gsub!(/\e\[3.m/) {|substring| substring[0..1] + "0;" + substring [2..4]} #convert 8bit ASCII colours to ANSI
-        line.gsub!("\e[27m","") #suppress invert
-        line.gsub!("\e[7m","")  #suppress reverse
-        line.gsub!("\e[0;37m","\e[1;37m")  #tweak colours
-        line.gsub!("\e[0;36m","\e[1;36m")  #tweak colours
-
         unless line.squish == "Ok."
           unless line.squish == "" || line == "\n"
-            line = ansi_to_bbcode(line)
             line.gsub!("\"", "'")
           end
           stripped_lines << line
@@ -299,9 +199,9 @@ module DiscourseFrotz
       end
 
       if save_location.blank?
-        output, s = Open3.capture2("#{SiteSetting.frotz_dumb_executable_directory}/./dfrotz -f ansi -i -Z 0 #{story_path}", :stdin_data=>input_data, :binmode=>true )
+        output, s = Open3.capture2("#{SiteSetting.frotz_dumb_executable_directory}/./dfrotz -f bbcode -i -Z 0 #{story_path}", :stdin_data=>input_data, :binmode=>true )
       else
-        output, s = Open3.capture2("#{SiteSetting.frotz_dumb_executable_directory}/./dfrotz -L #{save_location} -f ansi -i -Z 0 #{story_path}", :stdin_data=>input_data, :binmode=>true )
+        output, s = Open3.capture2("#{SiteSetting.frotz_dumb_executable_directory}/./dfrotz -L #{save_location} -f bbcode -i -Z 0 #{story_path}", :stdin_data=>input_data, :binmode=>true )
       end
 
       puts "BEFORE strip:\n"+output
