1340b17f4c75c35e4bebae5669ea2b0c3c241df7
Robert Barrow <merefield@gmail.com>
2022-02-23 18:37:42 +0000

    COMPATIBILITY: sidekiq now requires strict JSON arguments

--- lib/bot.rb
+++ lib/bot.rb
@@ -45,9 +45,9 @@ module DiscourseFrotz
     def invoke_background_job(job_class, opts)
       delay_in_seconds = DELAY_IN_SECONDS.to_i
       if delay_in_seconds > 0
-        job_class.perform_in(delay_in_seconds.seconds, opts)
+        job_class.perform_in(delay_in_seconds.seconds, opts.as_json)
       else
-        job_class.perform_async(opts)
+        job_class.perform_async(opts.as_json)
       end
     end
 
