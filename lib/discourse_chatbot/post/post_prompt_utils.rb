78ac9c70de165acd0dbb274f03b7f6d800298b08
merefield <merefield@gmail.com>
2023-03-05 22:36:42 +0000

    FIX: prompt picks wrong user for OP

--- lib/discourse_chatbot/post/post_prompt_utils.rb
+++ lib/discourse_chatbot/post/post_prompt_utils.rb
@@ -9,7 +9,7 @@ module ::DiscourseChatbot
       if SiteSetting.chatbot_open_ai_model == "gpt-3.5-turbo"
         messages = [{"role": "system", "content": I18n.t("chatbot.prompt.system")}]
         messages << {"role": "user", "content":  I18n.t("chatbot.prompt.title", topic_title: post_collection.first.topic.title)}
-        messages << {"role": "user", "content": I18n.t("chatbot.prompt.first_post", username: post_collection.first.user.username, raw: post_collection.first.topic.first_post.raw)}
+        messages << {"role": "user", "content": I18n.t("chatbot.prompt.first_post", username: post_collection.first.topic.first_post.user.username, raw: post_collection.first.topic.first_post.raw)}
 
         messages += post_collection.reverse.map { |p|
           {"role": (p.user_id == bot_user_id ? "assistant" : "user"), "content": (p.user_id == bot_user_id ? "#{p.raw}" : I18n.t("chatbot.prompt.post", username: p.user.username, raw: p.raw))}
