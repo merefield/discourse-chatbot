75a19caa1394503e8f8767a1571d45816e4da745
merefield <merefield@gmail.com>
2023-04-03 19:03:35 +0100

    FIX: post content reference

--- lib/discourse_chatbot/post/post_prompt_utils.rb
+++ lib/discourse_chatbot/post/post_prompt_utils.rb
@@ -16,7 +16,7 @@ module ::DiscourseChatbot
           post_content = p.raw
           post_content.gsub!(/\[quote.*?\](.*?)\[\/quote\]/m, '') if SiteSetting.chatbot_strip_quotes
           role = (p.user_id == bot_user_id ? "assistant" : "user")
-          content = (p.user_id == bot_user_id ? "#{p.raw}" : I18n.t("chatbot.prompt.post", username: p.user.username, raw: p.post_content))
+          content = (p.user_id == bot_user_id ? "#{p.raw}" : I18n.t("chatbot.prompt.post", username: p.user.username, raw: post_content))
           { "role": role , "content": content }
         end
 
