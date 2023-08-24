f35012b12fa0c5b2dfc20514589a3d7a7bef362f
merefield <merefield@gmail.com>
2023-08-24 09:56:19 +0100

    uprate vector search accuracy

--- db/migrate/20230820010105_create_chatbot_embeddings_index.rb
+++ db/migrate/20230820010105_create_chatbot_embeddings_index.rb
@@ -4,7 +4,7 @@ class CreateChatbotEmbeddingsIndex < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
       CREATE INDEX hnsw_index_on_chatbot_embeddings ON chatbot_embeddings USING hnsw(embedding)
-      WITH (dims=1536, m=8, efconstruction=8, efsearch=8);
+      WITH (dims=1536, m=32, efconstruction=32, efsearch=32);
     SQL
   end
 
