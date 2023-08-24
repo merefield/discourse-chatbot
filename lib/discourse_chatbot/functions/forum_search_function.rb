4a80d930ede7d29ab03ca4cf96f568dcf19f03ca
merefield <merefield@gmail.com>
2023-08-24 12:34:30 +0100

    improve data in post search response

--- lib/discourse_chatbot/functions/forum_search_function.rb
+++ lib/discourse_chatbot/functions/forum_search_function.rb
@@ -39,7 +39,9 @@ module DiscourseChatbot
           current_post = ::Post.find(result.to_i)
           url = "#{Discourse.current_hostname}/t/slug/#{current_post.topic_id}/#{current_post.post_number}"
           raw = current_post.raw
-          response += I18n.t("chatbot.prompt.function.forum_search.answer", url: url, raw: raw, rank: index + 1)
+          username = User.find(current_post.user_id).username
+          date = current_post.created_at.to_date
+          response += I18n.t("chatbot.prompt.function.forum_search.answer", url: url, username: username, date: date, raw: raw, rank: index + 1)
         end
         response
       rescue
