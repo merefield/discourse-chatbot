28338dc4074e9adc0563a920429275467b0087fe
merefield <merefield@gmail.com>
2023-09-02 10:01:37 +0100

    Add helpful notice

--- db/migrate/20230828010101_clear_chatbot_embeddings.rb
+++ db/migrate/20230828010101_clear_chatbot_embeddings.rb
@@ -3,5 +3,11 @@
 class ClearChatbotEmbeddings < ActiveRecord::Migration[7.0]
   def up
     ::DiscourseChatbot::PostEmbedding.delete_all
+    STDERR.puts "------------------------------DISCOURSE CHATBOT NOTICE----------------------------------"
+    STDERR.puts "This version of Chatbot introduces improvements to the selection of Posts for embedding."
+    STDERR.puts "         As such all existing chatbot post embeddings have been cleared out."
+    STDERR.puts "   Please refresh them inside the container with `rake chatbot:refresh_embeddings[1]`"
+    STDERR.puts "              Only necessary if you have selected bot type `agent`"
+    STDERR.puts "------------------------------DISCOURSE CHATBOT NOTICE----------------------------------"
   end
 end
