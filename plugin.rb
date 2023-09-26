716a80ae06ab33fe0a80be4d8d2df711552649f0
merefield <merefield@gmail.com>
2023-09-26 09:48:23 +0100

    bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-chatbot
 # about: a plugin that allows you to have a conversation with a configurable chatbot in Discourse Chat, Topics and Private Messages
-# version: 0.47
+# version: 0.48
 # authors: merefield
 # url: https://github.com/merefield/discourse-chatbot
 
