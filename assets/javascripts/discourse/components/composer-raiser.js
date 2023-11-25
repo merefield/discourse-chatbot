02b26fc4452bb4c520d5ae61fa54dfd60ad49e78
merefield <merefield@gmail.com>
2023-11-25 15:59:33 +0000

    open composer automatically for bot PMs

---
+++ assets/javascripts/discourse/components/composer-raiser.js
@@ -0,0 +1,33 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import Composer from "discourse/models/composer";
+
+export default class ComposerRaiserCompopnent extends Component {
+  @service siteSettings;
+  @service currentUser;
+  @service composer;
+
+  BOT_USER_ID = -4;
+
+  @action
+  raiseComposer() {
+    console.log(this.args.model)
+
+    this.composer.focusComposer({
+      fallbackToNewTopic: true,
+      openOpts: {
+        action: Composer.REPLY,
+        recipients: this.siteSettings.chatbot_bot_user,
+        draftKey: this.args.model.get("draft_key"),
+        topic: this.args.model,
+        hasGroups: false,
+        warningsDisabled: true,
+      },
+    });
+  }
+
+  get isBotConversation() {
+    return (this.currentUser && this.args.model.user_id === this.BOT_USER_ID)
+  }
+}
