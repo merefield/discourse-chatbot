6a6bf52b7e19b56b90e7952c71f18f34cb546b05
merefield <merefield@gmail.com>
2023-12-05 13:36:04 +0000

    FEATURE: add bot vision support

--- lib/discourse_chatbot/message/message_prompt_utils.rb
+++ lib/discourse_chatbot/message/message_prompt_utils.rb
@@ -15,10 +15,12 @@ module ::DiscourseChatbot
         text = (cm.user_id == bot_user_id ? "#{cm.message}" : I18n.t("chatbot.prompt.post", username: username, raw: cm.message)) 
         content = []
         content << {"type": "text", "text": text}
-        cm.uploads.each do |ul|
-          if ["png", "webp", "jpg", "jpeg", "gif", "ico", "avif"].include? (ul.extension) 
-            url = Discourse.base_url + ul.url
-            content << { "type": "image_url", "image_url": { "url": url } }
+        if SiteSetting.chatbot_support_vision
+          cm.uploads.each do |ul|
+            if ["png", "webp", "jpg", "jpeg", "gif", "ico", "avif"].include? (ul.extension) 
+              url = Discourse.base_url + ul.url
+              content << { "type": "image_url", "image_url": { "url": url } }
+            end
           end
         end
         { "role": role, "content": content}
