a39cb1ad322973b659b6e9a7de4dcd36bda439a3
merefield <merefield@gmail.com>
2024-04-05 18:42:57 +0100

    FIX: rubocopped test

--- spec/lib/event_evaluation_spec.rb
+++ spec/lib/event_evaluation_spec.rb
@@ -80,7 +80,7 @@ describe ::DiscourseChatbot::EventEvaluation do
     SiteSetting.chatbot_quota_low_trust = 1
 
     event = ::DiscourseChatbot::EventEvaluation.new
-    expect { event.over_quota(high_trust_user.id) }.to change { Topic.where(archetype: Archetype.private_message).count }.by(0)
+    expect { event.over_quota(high_trust_user.id) }.not_to change { Topic.where(archetype: Archetype.private_message).count }
     expect(event.over_quota(high_trust_user.id)).to equal(true)
   end
 
