35a99714ef04d5257e8b029e739b25727454fe12
merefield <merefield@gmail.com>
2024-04-07 00:41:51 +0100

    wip

--- lib/discourse_chatbot/functions/forum_search_function.rb
+++ lib/discourse_chatbot/functions/forum_search_function.rb
@@ -27,6 +27,7 @@ module DiscourseChatbot
     def process(args)
       begin
         super(args)
+        top_topics = []
         query = args[parameters[0][:name]]
         number_of_posts = args[parameters[1][:name]].blank? ? 3 : args[parameters[1][:name]]
         number_of_posts = number_of_posts > SiteSetting.chatbot_forum_search_function_max_results ? SiteSetting.chatbot_forum_search_function_max_results : number_of_posts
@@ -36,20 +37,63 @@ module DiscourseChatbot
 
         top_results = results[0..(number_of_posts - 1)]
 
-        response = I18n.t("chatbot.prompt.function.forum_search.answer_summary", number_of_posts: number_of_posts)
+        # exclude if not in scope for embeddings (job hasn't caught up yet)
+        top_results.select { |result| !::DiscourseChatbot::PostEmbeddingProcess.new.in_scope(result[:post_id]) || !::DiscourseChatbot::PostEmbeddingProcess.new.is_valid( result[:post_id])}
 
-        top_results.each_with_index do |result, index|
-          current_post = ::Post.find(result[:post_id].to_i)
-          # exclude if not in scope for embeddings (job hasn't caught up yet)
-          next if !::DiscourseChatbot::PostEmbeddingProcess.new.in_scope(current_post.id) || !::DiscourseChatbot::PostEmbeddingProcess.new.is_valid(current_post.id)
+        if SiteSetting.chatbot_forum_search_function_results_content_type != "matching_post"
+          top_topics = top_results.map { |result| ::Post.find(result[:post_id].to_i).topic_id }.uniq
+          response = I18n.t("chatbot.prompt.function.forum_search.answer.topic.summary", number_of_topics: top_topics.length)
 
-          score = result[:score]
-          url = "https://#{Discourse.current_hostname}/t/slug/#{current_post.topic_id}/#{current_post.post_number}"
-          raw = current_post.raw
-          username = User.find(current_post.user_id).username
-          date = current_post.created_at.to_date
-          response += I18n.t("chatbot.prompt.function.forum_search.answer", url: url, username: username, date: date, raw: raw, score: score, rank: index + 1)
+          accepted_post_types = SiteSetting.chatbot_include_whispers_in_post_history ? ::DiscourseChatbot::POST_TYPES_INC_WHISPERS : ::DiscourseChatbot::POST_TYPES_REGULAR_ONLY
+
+          top_topics.each_with_index do |topic_id, index|
+            top_result = top_results.find do |result|
+              post_topic_id = ::Post.find(result[:post_id].to_i).topic_id
+              post_topic_id == topic_id
+            end
+            score = top_result[:score]
+            current_topic = ::Topic.find(topic_id)
+            url = "https://#{Discourse.current_hostname}/t/slug/#{current_topic.id}"
+            title = current_topic.title
+            response += I18n.t("chatbot.prompt.function.forum_search.topic.answer", url: url, title: title, score: score, rank: index + 1)
+            post_number = 1
+            while post_number <= SiteSetting.chatbot_forum_search_function_results_posts_count do
+              post = ::Post.find_by(topic_id: topic_id, post_number: post_number )
+              next if post.deleted_at || !accepted_post_types.includes?(post.post_type)
+              break if post.nil?
+              response += I18n.t("chatbot.prompt.function.forum_search.answer.topic.post_content", username: post.user.username, date: post.created_at, raw: post.raw)
+              post_number += 1
+            end
+          end
+
+        else
+          response = I18n.t("chatbot.prompt.function.forum_search.answer.post.summary", number_of_posts: number_of_posts)
+          top_results.each_with_index do |result, index|
+            current_post = ::Post.find(result[:post_id].to_i)
+            score = result[:score]
+            url = "https://#{Discourse.current_hostname}/t/slug/#{current_post.topic_id}/#{current_post.post_number}"
+            raw = current_post.raw
+            username = User.find(current_post.user_id).username
+            date = current_post.created_at.to_date
+            response += I18n.t("chatbot.prompt.function.forum_search.post.answer", url: url, username: username, date: date, raw: raw, score: score, rank: index + 1)
+          end
         end
+
+
+
+          case SiteSetting.chatbot_forum_search_function_results_content_type
+          when "matching_post"
+
+          else
+            topic = current_post.topic
+            title = topic.title
+            case SiteSetting.chatbot_forum_search_function_results_content_type
+            when "posts_topic_with_title"
+              response += I18n.t("chatbot.prompt.function.forum_search.answer_topic_with_title", title: title)
+            when "posts_topic_without_title"
+              response += I18n.t("chatbot.prompt.function.forum_search.answer_topic_without_title")
+            end
+          end
         response
       rescue
         I18n.t("chatbot.prompt.function.forum_search.error", query: args[parameters[0][:name]])
@@ -57,3 +101,18 @@ module DiscourseChatbot
     end
   end
 end
+
+
+chatbot_forum_search_function_results_content_type:
+client: false
+default: "matching_post"
+type: enum
+choices:
+  - "matching_post"
+  - "posts_topic_with_title"
+  - "posts_topic_without_title"
+chatbot_forum_search_function_results_posts_count:
+client: false
+default: 3
+min: 1
+max: 20
