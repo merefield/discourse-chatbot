95d03f8e952f76b1bfd2a263557dcb52e0d875c0
merefield <merefield@gmail.com>
2024-10-22 11:18:36 +0100

    add time remaining to reset

--- lib/discourse_chatbot/functions/remaining_bot_quota_function.rb
+++ lib/discourse_chatbot/functions/remaining_bot_quota_function.rb
@@ -4,6 +4,7 @@ require_relative '../function'
 
 module DiscourseChatbot
   class RemainingBotTokenQuotaFunction < Function
+    QUOTA_RESET_JOB = "Jobs::ChatbotQuotaReset"
 
     def name
       'remaining_bot_quota'
@@ -29,8 +30,11 @@ module DiscourseChatbot
         remaining_quota_field_name =  SiteSetting.chatbot_quota_basis == "queries" ? CHATBOT_REMAINING_QUOTA_QUERIES_CUSTOM_FIELD : CHATBOT_REMAINING_QUOTA_TOKENS_CUSTOM_FIELD
         remaining_quota = ::DiscourseChatbot::EventEvaluation.new.get_remaining_quota(user_id, remaining_quota_field_name)
 
+        reset_job = MiniScheduler::Manager.discover_schedules.find {|job| job.schedule_info.instance_variable_get(:@klass).to_s == QUOTA_RESET_JOB}.schedule_info
+        days_remaining = (Time.at(reset_job.instance_variable_get(:@next_run)).to_date - Time.zone.now.to_date).to_i
+
         {
-          answer: I18n.t("chatbot.prompt.function.remaining_bot_quota.answer", quota: remaining_quota, units: SiteSetting.chatbot_quota_basis ),
+          answer: I18n.t("chatbot.prompt.function.remaining_bot_quota.answer", quota: remaining_quota, units: SiteSetting.chatbot_quota_basis, days_remaining: days_remaining),
           token_usage: 0
         }
       rescue => e
