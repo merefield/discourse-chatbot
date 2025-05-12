106278fcecb3dfbd526a563b56d43cab122c0fa4
merefield <merefield@gmail.com>
2025-05-12 09:38:40 +0100

    Revert "remove commented gems"
    This reverts commit 2379b3e07b384e9339dd5ddd4cdf6ae05335ae48.

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,8 @@
 # authors: merefield
 # url: https://github.com/merefield/discourse-chatbot
 
+# gem 'mime-types-data', '3.2025.0507', { require: false }
+# gem 'mime-types', '3.7.0', { require: false }
 gem 'multipart-post', '2.4.0', { require: false }
 gem 'faraday-multipart', '1.0.4', { require: false }
 gem 'event_stream_parser', '1.0.0', { require: false }
