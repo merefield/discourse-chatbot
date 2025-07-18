9847351dbbc0805897cefce742d6be63770d348b
merefield <merefield@gmail.com>
2025-07-18 08:01:15 +0100

    CHORE: update dep to be inline with discourse core plugins

--- plugin.rb
+++ plugin.rb
@@ -5,7 +5,7 @@
 # authors: merefield
 # url: https://github.com/merefield/discourse-chatbot
 
-gem 'multipart-post', '2.4.0', { require: false }
+gem 'multipart-post', '2.2.3', { require: false }
 gem 'faraday-multipart', '1.0.4', { require: false }
 gem 'event_stream_parser', '1.0.0', { require: false }
 gem "ruby-openai", '8.1.0', { require: false }
