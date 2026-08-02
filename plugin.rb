18feaa17865351ae5c6ca80cf246e12092a0f79d
merefield <merefield@gmail.com>
2026-08-02 17:24:24 +0100

    CHORE: Bump plugin version to 2.1.0

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-chatbot
 # about: a plugin that allows you to have a conversation with a configurable chatbot in Chat, Topics and Private Messages
-# version: 2.0.2
+# version: 2.1.0
 # authors: merefield
 # url: https://github.com/merefield/discourse-chatbot
 
